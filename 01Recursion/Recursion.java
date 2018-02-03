9492d895e0ed6bbdadfc9880791109de1e4ee351
Carmen <cbenitz@stuy.edu>
2018-02-02 21:19:18 -0500

    Fixed fibonacci and sqrt

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,39 +1,46 @@
+import java.lang.*;
 public class Recursion{
-    //1.1a factorial
-    public int fact(int n){
-	if(n < 0){
+  //1.1a factorial
+  public int fact(int n){
+    if(n < 0){
 	    throw new IllegalArgumentException();
-	}
-	if(n <= 1){
-	    return 1;
-	}
-	return fact(n - 1) * n;
     }
+    if(n <= 1){
+	    return 1;
+    }
+    return fact(n - 1) * n;
+  }
 
-    //1.1b Fibonacci
-    public int fib(int n){
-	if(n < 0){
+  //1.1b Fibonacci
+  public int fib(int n){
+    if(n < 0){
 	    throw new IllegalArgumentException();
-	}
-	if(n == 0){
-	    return 0;
-	}
-	if(n == 1){
-	    return 1;
-	}
-	return fib(n - 1) + fib(n - 2);
     }
+    return fibHelp(0,1,n);
+  }
 
-    public int fibHelp(int o, int p, int t){
-	return 0;
+  private int fibHelp(int low, int total, int n){
+    if(n == 0){
+      return total;
     }
+    return fibHelp(total, total + low, n-1);
+  }
 
-    //1.1c Square Root go in terms of percent diff
-    public double sqrt(double n){
-	return 0.0;
+  //1.1c Square Root go in terms of percent diff
+  public double sqrt(double n){
+    if(n < 0){
+	    throw new IllegalArgumentException();
     }
-
-    public static void main(String[] args){
-
+    if(n == 0){
+      return 0;
     }
+    return sqrtHelp(n, n/2);
+  }
+
+  private double sqrtHelp(double n, double guess){
+    if(Math.abs(((guess*guess - n)/ n)) < 0.0000000001){
+      return guess;
+    }
+    return sqrtHelp(n, (((n / guess) + guess) / 2));
+  }
 }
