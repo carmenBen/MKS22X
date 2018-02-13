b32e88c0f544783c81b276b09a184aeb61d1411b
carmenBen <cbenitz@stuy.edu>
2018-02-13 14:46:45 -0500

    Created toString and Constructor and exceptions

---
+++ 03Knight/Driver.java
@@ -0,0 +1,6 @@
+public class Driver{
+    public static void main(String[] args){
+	KnightBoard test = new KnightBoard(2,4);
+	System.out.println(test.solve(1,1));
+    }
+}
--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,14 +1,83 @@
-public KnightBoard(int startingRows,int startingCols) {}
-//non square is required!
+public class KnightBoard{
+    private int[][] board;
+    private int counter;
 
-public String toString() {}
-/*see format for toString below
-  blank boards use underscores 
-  you get a blank board if you never called solve or 
-  when there is no solution */
+    public KnightBoard(int startingRows,int startingCols) {
+	if(startingRows < 0 || startingCols < 0){
+	    throw new IllegalStateException();
+	}
+	board = new int[startingRows][startingCols];
+	counter = 0;
+        for(int r = 0;r < startingRows;r++){
+	    for(int c = 0;c < startingCols;c++){
+		board[r][c] = 0;
+	    }
+	}
+    }
+    
+    public String toString() {
+	String total = "";
+	for(int r = 0;r < board.length;r++){
+	    for(int c = 0;c < board[0].length;c++){
+		if(board.length * board[0].length >= 10){
+		    if(board[r][c] == 0){
+			total += " __";
+		    }
+		    else if(board[r][c] < 10){
+			total =total + " _" + board[r][c];
+		    }
+		    else{
+			total = total + " " + board[r][c];
+		    }
+		}
+		else{
+		    if(board[r][c] == 0){
+			total += " _";
+		    }
+		    else{
+			total = total + " " + board[r][c];
+		    }
+		}
+	    }
+	    total += "\n";
+	}
+	return total;
+    }
+    
+    public boolean solve(int row, int col) {
+	for(int r = 0;r < board.length;r++){
+	    for(int c = 0;c < board[0].length;c++){
+		if(board[r][c] != 0){
+		    throw new IllegalStateException();
+		}
+	    }
+	}
+	if(row < 0 || row >= board.length || col < 0 || col >= board[0].length){
+	    throw new IllegalArgumentException();
+	}
+	if(board.length == 1 ||board.length == 2|| board.length == 4|| board.length % 2 == 0 && board[0].length % 2 == 0|| board.length == 3 && (board[0].length == 4 || board[0].length == 6|| board[0].length ==  8)){
+	    return false;
+	}
+	return true;
+    }
+    
+    public int countSolutions() {
+	for(int r = 0;r < board.length;r++){
+	    for(int c = 0;c < board[0].length;c++){
+		if(board[r][c] != 0){
+		    throw new IllegalStateException();
+		}
+	    }
+	}
+	return 0;
+    }
 
-public boolean solve() {}
-public int     countSolutions() {}
+    private boolean solveH(int row ,int col, int level) {
+	if(level > row * col){
+	    return true;
+	}
+	return false;
+    }
+    // level is the # of the knight
 
-private boolean solveH(int row ,int col, int level) {}
-// level is the # of the knight
+}
