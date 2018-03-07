0ede0bed928720798cffa74af099b09b7d4c8754
carmenBen <cbenitz@stuy.edu>
2018-03-07 14:47:35 -0500

    Finished reading out of file. Began working on stomp instructins

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class USACO{
     public static int bronze(String filename){
+	//read file in
 	int[][] data;
 	ArrayList<String> lines = new ArrayList<String> ();
 	int[] rcen = new int[4];
@@ -12,6 +13,15 @@ public class USACO{
 	    for(int i = 0;i < 4;i++){
 		rcen[i] = inf.nextInt();
 	    }
+	    data = new int[rcen[0]][rcen[1]];
+	    String[] current = new String[rcen[1]];
+	    inf.nextLine();
+	    for(int r = 0;r < rcen[0];r++){
+		current = inf.nextLine().split(" ");
+		for(int c = 0;c < rcen[1];c++){
+		    data[r][c] = Integer.parseInt(current[c]);
+		}
+	    }
 	    while(inf.hasNextLine()){
 		lines.add(inf.nextLine());
 	    }
@@ -19,14 +29,31 @@ public class USACO{
 	    System.out.println("File not found: " + filename);
 	    System.exit(1);
 	}
-	System.out.println(rcen[0]);/*
-				      data = new int[lines.size()][lines.get(0).length()];
-	for(int r = 0;r<lines.size();r++){
-	    for(int c= 0;c<lines.get(0).length();c++){
-	        int current = lines.get(r).charAt(c);
-		data[r][c] = current;
+
+	//follow instructions in file
+	for(int i = 0;i < lines.size();i++){
+	    String[] current = new String[3];
+	    current = lines.get(i).split(" ");
+	    int highest = 0;
+	    for(int r = current[0];r < current[0] + 3;r++){
+		for(int c = current[1];c< current[1] + 3;c++){
+		    if(data[r][c] > highest){
+			highest = data[r][c];
+		    }
+		}
 	    }
-	    }*/
+	    while(current[2] > -1){
+		for(int r = current[0];r < current[0] + 3;r++){
+		    for(int c = current[1];c< current[1] + 3;c++){
+			if(
+			   }
+		    }
+		}
+	    }
+	    //change depth
+
+		    //calculate volume and return
+	
 	return 0;
     }
     //public static int silver(String filename);
