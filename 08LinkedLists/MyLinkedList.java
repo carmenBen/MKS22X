0b4d0aa7c09fcb7c5385ae4aac1224a3d382afca
Carmen <cbenitz@stuy.edu>
2018-04-09 19:00:39 -0400

    moved Node to inside of MyLinkedList

--- 08LinkedLists/MyLinkedList.java
+++ 08LinkedLists/MyLinkedList.java
@@ -1,170 +1,212 @@
 public class MyLinkedList{
-  Node first;
-  Node last;
-  int length;
 
-  public MyLinkedList(){
-    length = 0;
-    first = null;
-    last = null;
-  }
+  private class Node{
+    private Node next = null;
+    private Node prev = null;
+    private Integer data;
 
-  public boolean add(Integer value){
-    Node val = new Node(value,null,null);
-    if(length != 0){
-	    last.setNext(val);
-	    val.setPrev(last);
-    }
-    if(length == 0){
-	    first = val;
-    }
-    last = val;
-    length++;
-    return true;
-  }
+    public Node(Integer val, Node nextNode, Node prevNode){
+        next = nextNode;
+        prev = prevNode;
+        data = val;
+      }
+
     
-  public int size(){
-    return length;
-  }
+      public Node getNext(){
+        return next;
+      }
 
-  public String toString(){
-    if(length > 1 ){
-	    String total = "["+first.getValue() + ",";
-	    Node current = first.getNext();
-	    for(int i = 1;i < length-1;i++){
-        total += current.getValue() + ",";
+      public Node getPrev(){
+        return prev;
+      }
+
+      public Integer getValue(){
+        return data;
+      }
+
+      public void setNext(Node n){
+        next = n;
+      }
+
+      public void setPrev(Node n){
+        prev = n;;
+      }
+
+      public void setValue(Integer d){
+        data = d;
+      }
+
+      public String toString(){
+        return "" + data;
+      }
+  }
+  
+      Node first;
+    Node last;
+    int length;
+
+    public MyLinkedList(){
+      length = 0;
+      first = null;
+      last = null;
+    }
+
+    public boolean add(Integer value){
+      Node val = new Node(value,null,null);
+      if(length != 0){
+        last.setNext(val);
+        val.setPrev(last);
+      }
+      if(length == 0){
+        first = val;
+      }
+      last = val;
+      length++;
+      return true;
+    }
+    
+    public int size(){
+      return length;
+    }
+
+    public String toString(){
+      if(length > 1 ){
+        String total = "["+first.getValue() + ",";
+        Node current = first.getNext();
+        for(int i = 1;i < length-1;i++){
+          total += current.getValue() + ",";
+          current = current.getNext();
+        }
+        return total+last.getValue()+"]";
+      }
+      else if(length == 1){
+        return "[" + first.getValue() + "]";
+      }
+      else{
+        return "[]";
+      }
+    }
+
+    public Integer get(int index){
+      if(index >= length || index < 0){
+        throw new IndexOutOfBoundsException();      
+      }
+      return getNode(index).getValue();
+    }
+
+    private Node getNode(int n){
+      int i = 0;
+      Node current = first;
+      while(i <= n && current != null){
+        if(i==n){
+          return current;
+        }
+        i++;
         current = current.getNext();
-	    }
-	    return total+last.getValue()+"]";
+      }
+      return null;
     }
-    else if(length == 1){
-	    return "[" + first.getValue() + "]";
-    }
-    else{
-	    return "[]";
-    }
-  }
 
-  public Integer get(int index){
-    if(index >= length || index < 0){
-      throw new IndexOutOfBoundsException();      
+    public Integer set(int index,Integer newValue){
+      if(index >= length || index < 0){
+        throw new IndexOutOfBoundsException();      
+      }
+      Node current = 	getNode(index);
+      Integer old = current.getValue();
+      current.setValue(newValue);
+      return old;
     }
-    return getNode(index).getValue();
-  }
 
-  private Node getNode(int n){
-    int i = 0;
-    Node current = first;
-    while(i <= n && current != null){
-	    if(i==n){
-        return current;
-	    }
-	    i++;
-	    current = current.getNext();
+    public int indexOf(Integer value){
+      Node node = first;
+      int count = 0;
+      while(node != null){
+        if(node.getValue().equals(value)){
+          return count;
+        }
+        count++;
+        node = node.getNext();
+      }
+      return -1;
     }
-    return null;
-  }
-
-  public Integer set(int index,Integer newValue){
-    if(index >= length || index < 0){
-      throw new IndexOutOfBoundsException();      
-    }
-    Node current = 	getNode(index);
-    Integer old = current.getValue();
-    current.setValue(newValue);
-    return old;
-  }
-
-  public int indexOf(Integer value){
-    Node node = first;
-    int count = 0;
-    while(node != null){
-	    if(node.getValue().equals(value)){
-        return count;
-	    }
-	    count++;
-	    node = node.getNext();
-    }
-    return -1;
-  }
 
     public void add(int index, Integer value){//exceptions!
-	if(index > length || index < 0){
-	    throw new IndexOutOfBoundsException();      
-	}
-        if(length == 0){
-	    Node node = new Node(value,null,null);
-	    first = node;
-	    last = node;
-	}
-	else if(index != 0 && index != length){
-	    Node current = getNode(index);
-	    Node node = new Node(value,current,current.getPrev());
-	    current.getPrev().setNext(node);
-	    current.setPrev(node);
-	}
-	else if(index == length){
-	    Node node = new Node(value,null,last);
-	    last.setNext(node);
-	    last = node;
-	}
-	else{
-	    Node node = new Node(value,first,null);
-	    first.setPrev(node);
-	    first = node;
-	}
-    length++;
-  }
+      if(index > length || index < 0){
+        throw new IndexOutOfBoundsException();      
+      }
+      if(length == 0){
+        Node node = new Node(value,null,null);
+        first = node;
+        last = node;
+      }
+      else if(index != 0 && index != length){
+        Node current = getNode(index);
+        Node node = new Node(value,current,current.getPrev());
+        current.getPrev().setNext(node);
+        current.setPrev(node);
+      }
+      else if(index == length){
+        Node node = new Node(value,null,last);
+        last.setNext(node);
+        last = node;
+      }
+      else{
+        Node node = new Node(value,first,null);
+        first.setPrev(node);
+        first = node;
+      }
+      length++;
+    }
  	
 
-  public boolean remove(Integer value){
-    int index = indexOf(value);
-    if(index != 0 && index != length -1){
-	    Node node = getNode(index);
-	    node.getPrev().setNext(node.getNext());
-	    node.getNext().setPrev(node.getPrev());
+    public boolean remove(Integer value){
+      int index = indexOf(value);
+      if(index != 0 && index != length -1){
+        Node node = getNode(index);
+        node.getPrev().setNext(node.getNext());
+        node.getNext().setPrev(node.getPrev());
+      }
+      else if(index == length - 1){
+        last.getPrev().setNext(null);
+        last = last.getPrev();
+      }
+      else{
+        first.getNext().setPrev(null);
+        first =first.getNext();
+      }
+      length--;
+      return true;
     }
-    else if(index == length - 1){
-	    last.getPrev().setNext(null);
-	    last = last.getPrev();
-    }
-    else{
-	    first.getNext().setPrev(null);
-	    first =first.getNext();
-    }
-    length--;
-    return true;
-  }
 
-  public Integer remove(int index){//exceptions!
-    if(index >= length || index < 0){
-      throw new IndexOutOfBoundsException();      
+    public Integer remove(int index){//exceptions!
+      if(index >= length || index < 0){
+        throw new IndexOutOfBoundsException();      
+      }
+      Node node;
+      if(index != 0 && index != length -1){
+        node = getNode(index);
+        node.getPrev().setNext(node.getNext());
+        node.getNext().setPrev(node.getPrev());
+      }
+      else if(index == length - 1){
+        node = getNode(index);
+        last.getPrev().setNext(null);
+        last = last.getPrev();
+      }
+      else{
+        node = getNode(index);
+        first.getNext().setPrev(null);
+        first =first.getNext();
+      }
+      length--;
+      return node.getValue();
     }
-    Node node;
-    if(index != 0 && index != length -1){
-	    node = getNode(index);
-	    node.getPrev().setNext(node.getNext());
-	    node.getNext().setPrev(node.getPrev());
-    }
-    else if(index == length - 1){
-	    node = getNode(index);
-	    last.getPrev().setNext(null);
-	    last = last.getPrev();
-    }
-    else{
-	node = getNode(index);
-	    first.getNext().setPrev(null);
-	    first =first.getNext();
-    }
-    length--;
-    return node.getValue();
-  }
 
     
-  public void clear(){
-    first = null;
-    last = null;
-    length = 0;
-  }
+    public void clear(){
+      first = null;
+      last = null;
+      length = 0;
+    }
 
-}
+  }
