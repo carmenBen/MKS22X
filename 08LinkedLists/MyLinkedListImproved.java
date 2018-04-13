45a1bfd0cce8055e0f2bec0c1916f9f38e833123
Carmen <cbenitz@stuy.edu>
2018-04-12 21:14:04 -0400

    Finished radix sort

--- 08LinkedLists/MyLinkedListImproved.java
+++ 08LinkedLists/MyLinkedListImproved.java
@@ -1,312 +1,328 @@
 import java.util.*;
 
 public class MyLinkedListImproved<T extends Comparable<T>> implements Iterable<T>{
-    Node first;
-    Node last;
-    int length;
+  Node first;
+  Node last;
+  int length;
 
-    private class MyLinkedListIterator implements Iterator<T>{
-	Node node;
-	T current;
+  private class MyLinkedListIterator implements Iterator<T>{
+    Node node;
+    T current;
 
-	public MyLinkedListIterator(Node info){
+    public MyLinkedListIterator(Node info){
 	    node = info;
-	}
-
-	public void remove(){
-	    throw new UnsupportedOperationException();
-	}
-
-	public T next(){
-	    if(hasNext()){
-		current = node.getValue();
-		node = node.getNext();
-		return current;
-	    }
-	    else{
-		throw new NoSuchElementException();
-	    }
-	}
-
-	public boolean hasNext(){
-	    return node != null;
-	}
     }
 
-  
-    private class Node{
-	private Node next = null;
-	private Node prev = null;
-	private T data;
+    public void remove(){
+	    throw new UnsupportedOperationException();
+    }
 
-	public Node(T val, Node nextNode, Node prevNode){
+    public T next(){
+	    if(hasNext()){
+        current = node.getValue();
+        node = node.getNext();
+        return current;
+	    }
+	    else{
+        throw new NoSuchElementException();
+	    }
+    }
+
+    public boolean hasNext(){
+	    return node != null;
+    }
+  }
+
+  
+  private class Node{
+    private Node next = null;
+    private Node prev = null;
+    private T data;
+
+    public Node(T val, Node nextNode, Node prevNode){
 	    next = nextNode;
 	    prev = prevNode;
 	    data = val;
-	}
+    }
 
     
-	public Node getNext(){
+    public Node getNext(){
 	    return next;
-	}
+    }
 
-	public Node getPrev(){
+    public Node getPrev(){
 	    return prev;
-	}
+    }
 
-	public T getValue(){
+    public T getValue(){
 	    return data;
-	}
+    }
 
-	public void setNext(Node n){
+    public void setNext(Node n){
 	    next = n;
-	}
+    }
 
-	public void setPrev(Node n){
+    public void setPrev(Node n){
 	    prev = n;;
-	}
+    }
 
-	public void setValue(T d){
+    public void setValue(T d){
 	    data = d;
-	}
-
-	public String toString(){
-	    return "" + data;
-	}
     }
-  
-    public MyLinkedListImproved(){
-	length = 0;
-	first = null;
-	last = null;
-    }
-
-    public Iterator<T> iterator(){
-	return new MyLinkedListIterator(first);
-    }
-  
-    public boolean add(T value){
-	Node val = new Node(value,null,null);
-	if(length != 0){
-	    last.setNext(val);
-	    val.setPrev(last);
-	}
-	if(length == 0){
-	    first = val;
-	}
-	last = val;
-	length++;
-	return true;
-    }
-    
-    public int size(){
-	return length;
-    }
-
-    public void extend(MyLinkedListImproved<T> O){
-	last.setNext(O.getNode(0));
-	O.getNode(0).setPrev(last);
-	length += O.size();
-	O.clear();
-    }
-
 
     public String toString(){
-	if(length > 1 ){
+	    return "" + data;
+    }
+  }
+  
+  public MyLinkedListImproved(){
+    length = 0;
+    first = null;
+    last = null;
+  }
+  public Node getLast(){
+    return last;
+  }
+
+  public Iterator<T> iterator(){
+    return new MyLinkedListIterator(first);
+  }
+  
+  public boolean add(T value){
+    Node val = new Node(value,null,null);
+    if(length != 0){
+	    last.setNext(val);
+	    val.setPrev(last);
+    }
+    if(length == 0){
+	    first = val;
+    }
+    last = val;
+    length++;
+    return true;
+  }
+    
+  public int size(){
+    return length;
+  }
+
+  public void extend(MyLinkedListImproved<T> O){
+    if(this.size() <= 0 && O.size() != 0){
+      first = O.getNode(0);
+      last = O.getLast();
+      length =O.size();
+      O.clear();
+    }
+    else if(O.size() != 0){
+      last.setNext(O.getNode(0));
+      O.getNode(0).setPrev(last);
+      last = O.getLast();
+      length += O.size();
+      O.clear();
+    }
+  }
+
+
+  public String toString(){
+    if(length > 1 ){
 	    String total = "["+first.getValue() + ",";
 	    Node current = first.getNext();
 	    for(int i = 1;i < length-1;i++){
-		total += current.getValue() + ",";
-		current = current.getNext();
+        total += current.getValue() + ",";
+        current = current.getNext();
 	    }
 	    return total+last.getValue()+"]";
-	}
-	else if(length == 1){
+    }
+    else if(length == 1){
 	    return "[" + first.getValue() + "]";
-	}
-	else{
+    }
+    else{
 	    return "[]";
-	}
     }
+  }
 
-    public T get(int index){
-	if(index >= length || index < 0){
+  public T get(int index){
+    if(index >= length || index < 0){
 	    throw new IndexOutOfBoundsException();      
-	}
-	return getNode(index).getValue();
     }
+    return getNode(index).getValue();
+  }
 
-    public Node getNode(int n){
-	int i = 0;
-	Node current = first;
-	while(i <= n && current != null){
+  public Node getNode(int n){
+    int i = 0;
+    Node current = first;
+    while(i <= n && current != null){
 	    if(i==n){
-		return current;
+        return current;
 	    }
 	    i++;
 	    current = current.getNext();
-	}
-	return null;
     }
+    return null;
+  }
 
-    public T set(int index,T newValue){
-	if(index >= length || index < 0){
+  public T set(int index,T newValue){
+    if(index >= length || index < 0){
 	    throw new IndexOutOfBoundsException();      
-	}
-	Node current = getNode(index);
-	T old = current.getValue();
-	current.setValue(newValue);
-	return old;
     }
+    Node current = getNode(index);
+    T old = current.getValue();
+    current.setValue(newValue);
+    return old;
+  }
 
-    public int indexOf(T value){
-	Node node = first;
-	int count = 0;
-	while(node != null){
+  public int indexOf(T value){
+    Node node = first;
+    int count = 0;
+    while(node != null){
 	    if(node.getValue().equals(value)){
-		return count;
+        return count;
 	    }
 	    count++;
 	    node = node.getNext();
-	}
-	return -1;
     }
+    return -1;
+  }
 
-    public void add(int index, T value){//exceptions!
-	if(index > length || index < 0){
+  public void add(int index, T value){//exceptions!
+    if(index > length || index < 0){
 	    throw new IndexOutOfBoundsException();      
-	}
-	if(length == 0){
+    }
+    if(length == 0){
 	    Node node = new Node(value,null,null);
 	    first = node;
 	    last = node;
-	}
-	else if(index != 0 && index != length){
+    }
+    else if(index != 0 && index != length){
 	    Node current = getNode(index);
 	    Node node = new Node(value,current,current.getPrev());
 	    current.getPrev().setNext(node);
 	    current.setPrev(node);
-	}
-	else if(index == length){
+    }
+    else if(index == length){
 	    Node node = new Node(value,null,last);
 	    last.setNext(node);
 	    last = node;
-	}
-	else{
+    }
+    else{
 	    Node node = new Node(value,first,null);
 	    first.setPrev(node);
 	    first = node;
-	}
-	length++;
     }
+    length++;
+  }
  	
 
-    public boolean remove(T value){
-	int index = indexOf(value);
-	if(index != 0 && index != length -1){
+  public boolean remove(T value){
+    int index = indexOf(value);
+    if(length == 1 || length == 0){
+      clear();
+      return true;
+    }
+    else if(index != 0 && index != length -1){
 	    Node node = getNode(index);
 	    node.getPrev().setNext(node.getNext());
 	    node.getNext().setPrev(node.getPrev());
-	}
-	else if(index == length - 1){
+    }
+    else if(index == length - 1){
 	    last.getPrev().setNext(null);
 	    last = last.getPrev();
-	}
-	else{
-	    first.getNext().setPrev(null);
-	    first =first.getNext();
-	}
-	length--;
-	return true;
     }
+    else{
+	    first =first.getNext();
+	    first.setPrev(null);
+    }
+    length--;
+    return true;
+  }
 
-    public T remove(int index){//exceptions!
-	if(index >= length || index < 0){
+  public T remove(int index){//exceptions!
+    if(index >= length || index < 0){
 	    throw new IndexOutOfBoundsException();      
-	}
-	Node node;
-	if(index != 0 && index != length -1){
+    }
+    Node node;
+    if(index != 0 && index != length -1){
 	    node = getNode(index);
 	    node.getPrev().setNext(node.getNext());
 	    node.getNext().setPrev(node.getPrev());
-	}
-	else if(index == length - 1){
+    }
+    else if(index == length - 1){
 	    node = getNode(index);
 	    last.getPrev().setNext(null);
 	    last = last.getPrev();
-	}
-	else{
+    }
+    else{
 	    node = getNode(index);
 	    first.getNext().setPrev(null);
 	    first =first.getNext();
-	}
-	length--;
-	return node.getValue();
     }
+    length--;
+    return node.getValue();
+  }
 
     
-    public void clear(){
-	first = null;
-	last = null;
-	length = 0;
-    }
+  public void clear(){
+    first = null;
+    last = null;
+    length = 0;
+  }
     
-    public int max(){
-	if(length == 0){
+  public int max(){
+    if(length == 0){
 	    return -1;
-	}
-	Node node = first;
-	int maxIndex = 0;
-	int i = 0;
-	T max = node.getValue();
-	while(node != null){
+    }
+    Node node = first;
+    int maxIndex = 0;
+    int i = 0;
+    T max = node.getValue();
+    while(node != null){
 	    if(node.getValue().compareTo(max) > 0){
-		maxIndex = i;
-		max = node.getValue();
+        maxIndex = i;
+        max = node.getValue();
 	    }
 	    i++;
 	    node = node.getNext();
-	}
-	return maxIndex;
     }
+    return maxIndex;
+  }
 
-    public int min(){
-	if(length == 0){
+  public int min(){
+    if(length == 0){
 	    return -1;
-	}
-	Node node = first;
-	int minIndex = 0;
-	int i = 0;
-	T min = node.getValue();
-	while(node != null){
+    }
+    Node node = first;
+    int minIndex = 0;
+    int i = 0;
+    T min = node.getValue();
+    while(node != null){
 	    if(node.getValue().compareTo(min) < 0){
-		minIndex = i;
-		min = node.getValue();
+        minIndex = i;
+        min = node.getValue();
 	    }
 	    i++;
 	    node = node.getNext();
-	}
-	return minIndex;
     }
+    return minIndex;
+  }
     
 
-    public static void main(String[] args){
-	MyLinkedListImproved<Integer> a = new MyLinkedListImproved<>();
-	a.add(Integer.valueOf(0));
-	a.add(Integer.valueOf(5));
-	a.add(Integer.valueOf(2));
-	for(Integer n : a){
+  public static void main(String[] args){
+    MyLinkedListImproved<Integer> a = new MyLinkedListImproved<>();
+    a.add(Integer.valueOf(0));
+    a.add(Integer.valueOf(5));
+    a.add(Integer.valueOf(2));
+    for(Integer n : a){
 	    System.out.println(n);
-	}
-	System.out.println(a);
-
-	MyLinkedListImproved<Integer> b = new MyLinkedListImproved<>();
-	b.add(Integer.valueOf(0));
-	b.add(Integer.valueOf(5));
-	b.add(Integer.valueOf(2));
-	System.out.println(b);
-	a.extend(b);
-	System.out.println(a);
-	System.out.println(b);
     }
+    System.out.println(a);
+
+    MyLinkedListImproved<Integer> b = new MyLinkedListImproved<>();
+    b.add(Integer.valueOf(0));
+    b.add(Integer.valueOf(5));
+    b.add(Integer.valueOf(2));
+    System.out.println(b);
+    a.extend(b);
+    System.out.println(a);
+    System.out.println(b);
+  }
 }
--- 08LinkedLists/Sorts.java
+++ 08LinkedLists/Sorts.java
@@ -1,28 +1,42 @@
+import java.util.*;
 public class Sorts{
 
     public static void radixSort(MyLinkedListImproved<Integer> data){
         String max = "" +  data.get(data.max());
 	int maxi = max.length();
-	System.out.println(maxi);
-	MyLinkedListImproved<Integer>[] buckets = new MyLinkedListImproved<Integer>[10];
+	ArrayList<MyLinkedListImproved<Integer>> buckets = new ArrayList<MyLinkedListImproved<Integer>>();
+  for(int i = 0;i <10;i++){
+    buckets.add(new MyLinkedListImproved<Integer>());
+  }
 	for(int i = 0;i < maxi;i++){
 	    for(Integer n:data){
-		int l = n%(10*(i+1));
-		buckets[l].add(n);
-		data.remove(n);
+        String check = "" + n;
+        int l = 0;
+        if(check.length() - i - 1 >= 0){
+          l = check.charAt(check.length()-i-1) - '0';
+        }
+        buckets.get(l).add(n);
+        data.remove(n);
 	    }
 	    for(int j = 0;j < 10;j++){
-		data.extend(buckets[j]);
+        data.extend(buckets.get(j));
 	    }
 	}
     }
 
     public static void main(String[] args){
 	MyLinkedListImproved<Integer> a = new MyLinkedListImproved<>();
-	a.add(Integer.valueOf(0));
+	a.add(Integer.valueOf(1));
 	a.add(Integer.valueOf(533));
 	a.add(Integer.valueOf(12));
+	a.add(Integer.valueOf(1));
+	a.add(Integer.valueOf(533));
+	a.add(Integer.valueOf(12));
+  a.add(Integer.valueOf(44));
+	a.add(Integer.valueOf(238));
+	a.add(Integer.valueOf(999));
         radixSort(a);
+        System.out.println(a);
     }
 
 }
