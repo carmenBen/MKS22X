aa640448e87a4adbeafa7f3df59a222d2b1b0e7c
carmenBen <cbenitz@stuy.edu>
2018-04-27 14:41:53 -0400

    Fixed compare. Works completely for Strings

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -1,120 +1,125 @@
 import java.util.*;
 
 public class MyHeap{
-  private String[] data;
-  private int size;
-  private int max = 1;
+    private String[] data;
+    private int size;
+    private int max = 1;
 
-  public MyHeap(){
-    size = 0;
-    data = new String[10];
-  }
-
-  public MyHeap(boolean type){
-    size = 0;
-    if(type){
-      max = 1;
+    public MyHeap(){
+	size = 0;
+	data = new String[10];
     }
-    else{
-      max = -1;
+
+    public MyHeap(boolean type){
+	size = 0;
+	if(type){
+	    max = 1;
+	}
+	else{
+	    max = -1;
+	}
+	data = new String[10];
     }
-    data = new String[10];
-  }
 
-  public int size(){
-    return size;
-  }
-
-  public String peak(){
-    if(size >0){
-      return data[0];
+    public int size(){
+	return size;
     }
-    return "";
-  }
 
-  public String remove(){
-    String temp = data[0];
-    data[0] = data[size -  1];
-    data[size - 1] = null;
-    size--;
-    int current = 0;
-    // pick child
-    int child = (current *2) + 1;
-
-    while((child < size && compare(data[current],data[child])) ||(child+1<size && compare(data[current],data[child+1]))){
-      if(compare(data[current],data[child])){
-        swap(current,child);
-        current = child;
-      }
-      else{
-        swap(current,child+1);
-        current = child+1;
-      }
-      //System.out.println((current*2 + 1) + " " + size + data[current*2 + 2]);
-      child = (current*2) + 1;
+    public String peak(){
+	if(size >0){
+	    return data[0];
+	}
+	return "";
     }
-    return temp;
-  }
 
+    public String remove(){
+	String temp = data[0];
+	data[0] = data[size -  1];
+	data[size - 1] = null;
+	size--;
+	int current = 0;
+	// pick child
+	int child = (current *2) + 1;
 
-  public void add(String s){
-    if(data.length == size){
-      resize();
+	while((child < size && compare(data[current],data[child])) ||(child+1<size && compare(data[current],data[child+1]))){
+	    if(child+1>=size){
+		swap(current,child);
+		current = child;
+		return temp;
+	    }
+	    if(compare(data[child+1],data[child])){
+		swap(current,child);
+		current = child;
+	    }
+	    else{
+		swap(current,child+1);
+		current = child+1;
+	    }
+	    //System.out.println((current*2 + 1) + " " + size + data[current*2 + 2]);
+	    child = (current*2) + 1;
+	}
+	return temp;
     }
-    int current = size;
-    int parent = (size - 1) / 2;
-    data[size] = s;
-      System.out.println("parent ="+data[parent]+" child= "+data[current]);
-    while((parent >= 0) && compare(data[parent],data[current])){
-      swap(current,parent);
-      current = parent;
-      parent = (current - 1)/ 2;
+
+
+    public void add(String s){
+	if(data.length == size){
+	    resize();
+	}
+	int current = size;
+	int parent = (size - 1) / 2;
+	data[size] = s;
+	//    System.out.println("parent ="+data[parent]+" child= "+data[current]);
+	while((parent >= 0) && compare(data[parent],data[current])){
+	    swap(current,parent);
+	    current = parent;
+	    parent = (current - 1)/ 2;
+	}
+	size++;
+	//    System.out.println(Arrays.toString(data));
     }
-    size++;
-    System.out.println(Arrays.toString(data));
-  }
 
-  public void swap(int a, int b){
-    String temp = data[a];
-    data[a] = data[b];
-    data[b] = temp;
-  }
-
-  public void resize(){
-    String[] newData =  new String[size*2];
-    for(int i = 0;i < size;i++){
-      newData[i] = data[i];
+    public void swap(int a, int b){
+	String temp = data[a];
+	data[a] = data[b];
+	data[b] = temp;
     }
-    data = newData;
-  }
 
-  public boolean compare(String b, String a){
-    System.out.println(b+ " " + a+ " " + max);
-    if((b.compareTo(a)*max)  == -1){
-      System.out.println("1");
-      return true;
+    public void resize(){
+	String[] newData =  new String[size*2];
+	for(int i = 0;i < size;i++){
+	    newData[i] = data[i];
+	}
+	data = newData;
     }
-    System.out.println("3");
-    return false;
-  }
 
-  public static void main(String[] args){
-    MyHeap test = new MyHeap(true);
-    test.add("aaaaa");
-    test.add("a");
-    test.add("aaa");
-    test.add("aaaa");
-    test.add("aa");
-    test.add("aaaaaa");
-    System.out.println(test.peak());
-    test.remove();
-    System.out.println(test.peak());
-    test.remove();
-    System.out.println(test.peak());
-    test.remove();
-    System.out.println(test.peak());
-    test.remove();
-    System.out.println(test.peak());
-  }
+    public boolean compare(String b, String a){
+	//    System.out.println(b+ " " + a+ " " + max);
+	if((b.compareTo(a)*max)  < 0){
+	    //  System.out.println("1");
+	    return true;
+	}
+	//   System.out.println("3");
+	return false;
+    }
+
+    public static void main(String[] args){
+	MyHeap test = new MyHeap(false);
+	test.add("z");
+	test.add("a");
+	test.add("c");
+	test.add("d");
+	test.add("b");
+	test.add("k");
+	System.out.println(test.peak());
+	test.remove();
+	System.out.println(test.peak());
+	test.remove();
+	System.out.println(test.peak());
+	test.remove();
+	System.out.println(test.peak());
+	test.remove();
+	System.out.println(test.peak());
+    }
 
 }
