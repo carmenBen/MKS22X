674777481188575a8093871959bbb6268638ade5
carmenBen <cbenitz@stuy.edu>
2018-04-30 14:50:27 -0400

    Began heapSort

---
+++ 12MyHeap/Sorts.java
@@ -0,0 +1,17 @@
+public class Sorts{
+    public static void heapSort(Integer[] data){
+	//make array into heap. return?
+    }
+
+    public static void main(String[] args){
+	Integer[] test = new Integer[10];
+	test[0] = 5;
+	test[1] = 1;
+	test[2] = 3;
+	test[3] = 2;
+	test[4] = 4;
+	System.out.println(Arrays.toString(test));
+        heapSort(test);
+	System.out.println(Arrays.toString(test));
+    }
+}
